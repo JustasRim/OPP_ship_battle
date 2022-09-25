3dfe3655e907537e401aa83fbf257ddfd0f5b7c6
Justas Rimkus <jusrim3@ktu.lt>
2022-09-25 19:38:25 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shipBattle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
